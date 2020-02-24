7c080bef0f18919db9b8fb73ef63ecb4904c426f
kami_teru <teru.k.business@gmail.com>
2020-02-24 18:32:41 +0900

    Implements View code for Xamarin and UWP.

---
+++ MyApp.ViewModel/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MyApp.ViewModel
+{
+    public class MainViewModel: ReactiveObject
+    {
+
+        [Reactive]
+        public string Name { get; set; }
+
+        [Reactive]
+        public string Organization { get; set; }
+
+        public ICommand SubmitCommand { private set; get; }
+
+        public MainViewModel()
+        {
+            SubmitCommand = ReactiveCommand.CreateFromTask(ProcessSubmit);
+        }
+
+        private async Task ProcessSubmit()
+        {
+            await Task.Delay(1500);
+        }
+    }
+}
